dd8332da51c9500a2ddc8e1051c66d0e6d7ac985
Hpsiix <djalil-normal@hotmail.fr>
2022-08-28 20:06:13 +0200

    adding client

--- src/app/components/prestation-card/prestation-card.component.ts
+++ src/app/components/prestation-card/prestation-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 
 @Component({
   selector: 'app-prestation-card',
@@ -6,10 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./prestation-card.component.scss']
 })
 export class PrestationCardComponent implements OnInit {
-
+  @Input() code:string | undefined ;
+  image: string | undefined;
+  titre: string | undefined;
+  desc: string | undefined;
   constructor() { }
 
   ngOnInit(): void {
+    switch (this.code){
+      case "plombier": {
+        this.image="../../../assets/prestataires/plombier.webp";
+        this.titre="John Plombier"
+        break;
+      }
+      case "jardinier": {
+        this.image="../../../assets/presentation/pdp.webp";
+        this.titre="Billal Benyoub"
+        break;
+      }
+      case "macon": {
+        this.image="../../../assets/prestataires/macon.jpg";
+        this.titre="Bob"
+        break;
+      }
+      case "electricien": {
+        this.image="../../../assets/prestataires/electricien.webp";
+        this.titre="Lamine electro"
+        break;
+      }
+    }
   }
 
 }
